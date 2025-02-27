c5a9e33206d5e734833b7d7663f0cadee95c978e
FedeMFernandez <fedefernandez218@gmail.com>
2025-02-27 16:50:30 -0300

    Added list of heroes

---
+++ src/app/commons/services/notification.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from "@angular/core";
+import Swal, { SweetAlertIcon, SweetAlertResult } from "sweetalert2";
+
+@Injectable()
+export class NotificationService {
+  private readonly DefaultTitle: string = 'Aviso';
+
+  private toast: any;
+
+  constructor() {
+    this.toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 5000,
+      timerProgressBar: true,
+      didOpen: (toast:any) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+      },
+    });
+  }
+
+  show(message: string, icon: 'success' | 'info' | 'warning' | 'error' = 'success'): void {
+    this.toast.fire({
+      icon,
+      title: message,
+    });
+  }
+
+  showInfo(message: string): void {
+    this.dispatch(message, 'info');
+  }
+
+  showSuccess(message: string): void {
+    this.dispatch(message, 'success', 'Operación exitosa');
+  }
+
+  showWarning(message: string): void {
+    this.dispatch(message, 'warning', 'Alerta');
+  }
+
+  showError(message: string): void {
+    this.dispatch(message, 'error', 'Error');
+  }
+
+  async showQuestion(message: string): Promise<boolean> {
+    const result = await this.dispatchDialog(message);
+    return Promise.resolve(result.isConfirmed);
+  }
+
+  private dispatch(message: string, icon: SweetAlertIcon, title: string = this.DefaultTitle): void {
+    Swal.fire(title, message, icon);
+  }
+
+  private async dispatchDialog(message: string): Promise<SweetAlertResult> {
+    return await Swal.fire({
+      title: 'Atención',
+      text: message,
+      icon: 'question',
+      showCancelButton: true,
+      cancelButtonColor: '#eb445a',
+      confirmButtonColor: '#005cbb',
+      confirmButtonText: 'OK',
+      cancelButtonText: 'Cancelar',
+    });
+  }
+}
---
+++ src/app/commons/services/rest.service.ts
@@ -0,0 +1,81 @@
+
+import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { first } from "rxjs";
+import { environment } from "../../../environments/environment";
+
+
+@Injectable({
+    providedIn: 'root',
+})
+export class RestService {
+
+    constructor(
+        private httpClient: HttpClient,
+    ) { }
+
+    private urlCompose(endpoint: string): string {
+        return `${environment.apiURL}${endpoint}`;
+    }
+
+    private getHttpOptions(): Object {
+      return {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+        }),
+        withCredentials: false,
+      }
+    }
+
+    async get<T>(endpoint: string): Promise<T> {
+      return new Promise((resolve, reject) => {
+          this.httpClient.get(this.urlCompose(endpoint), this.getHttpOptions()).pipe(first()).subscribe({
+              next: this.successCallback<T>(resolve, reject),
+              error: this.errorCallback(reject),
+          })
+      });
+    }
+
+    async post<T>(endpoint: string, body: any): Promise<T> {
+      return new Promise((resolve, reject) => {
+          this.httpClient.post(this.urlCompose(endpoint), body, this.getHttpOptions()).pipe(first()).subscribe({
+              next: this.successCallback<T>(resolve, reject),
+              error: this.errorCallback(reject),
+          })
+      });
+    }
+
+    async put<T>(endpoint: string, body: any): Promise<T> {
+      return new Promise((resolve, reject) => {
+          this.httpClient.put(this.urlCompose(endpoint), body, this.getHttpOptions()).pipe(first()).subscribe({
+              next: this.successCallback<T>(resolve, reject),
+              error: this.errorCallback(reject),
+          })
+      });
+    }
+
+    async delete<T>(endpoint: string): Promise<T> {
+      return new Promise((resolve, reject) => {
+          this.httpClient.delete(this.urlCompose(endpoint), this.getHttpOptions()).pipe(first()).subscribe({
+              next: this.successCallback<T>(resolve, reject),
+              error: this.errorCallback(reject),
+          })
+      });
+    }
+
+    private successCallback<T>(resolve: any, reject: any): (data: any) => void {
+        return (data: any) => {
+            try {
+              resolve(data as T);
+            } catch (error: any) {
+              reject(error);
+            }
+        }
+    }
+
+    private errorCallback(reject: Function): (error: HttpErrorResponse) => void {
+        return (error: HttpErrorResponse) => {
+            reject(error);
+        }
+    }
+}
