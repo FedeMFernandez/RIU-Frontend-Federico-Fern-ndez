80857f390d450dc172717ab3a1a146c39be98a26
FedeMFernandez <fedefernandez218@gmail.com>
2025-03-03 05:45:09 -0300

    Removed unused css

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, NativeDateAdapter } from '@angular/material/core';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 import Formats from './core/constants/formats.constants';
